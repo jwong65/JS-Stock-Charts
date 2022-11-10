8d1f3032563a948dc8a2ddc316912df973326e9b
unknown <jason.wong6507@gmail.com>
2022-11-10 02:37:20 -0500

    Using the test to make sure I can iterate them later

--- public/index.js
+++ public/index.js
@@ -4,14 +4,21 @@ async function main() {
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
 
-
-    let stockResults = await fetch("https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX&interval=1min&apikey=beeaae5d371b40ff9bae68960148ca7d",
+//https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX&interval=30min&apikey=beeaae5d371b40ff9bae68960148ca7d
+    let stockResults = await fetch("https://api.twelvedata.com/time_series?symbol=AAPL,EUR/USD,IXIC&interval=1min&apikey=demo",
     {
-        header: Authorization="apikey beeaae5d371b40ff9bae68960148ca7d"
+        //header: Authorization="apikey beeaae5d371b40ff9bae68960148ca7d"
     })
 
     let stocksResults =await stockResults.json()
     console.log(stocksResults)
+
+    let AAPL = stocksResults.AAPL
+    console.log(AAPL)
+
+
+
+
 }
 
 main()
