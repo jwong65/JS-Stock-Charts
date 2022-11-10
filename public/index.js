21c88556bdf7f29232569ffc30d41ec9375a18f4
unknown <jason.wong6507@gmail.com>
2022-11-10 02:32:02 -0500

    It says I have run out of minutely API credits, so I'm using the test demo to make sure the JSON comes out

--- public/index.js
+++ public/index.js
@@ -5,8 +5,13 @@ async function main() {
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
 
 
-    let stockResults = await fetch("https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX&interval=1min&apikey=beeaae5d371b40ff9bae68960148ca7d")
-    console.log(stockResults)
+    let stockResults = await fetch("https://api.twelvedata.com/time_series?symbol=GME,MSFT,DIS,BNTX&interval=1min&apikey=beeaae5d371b40ff9bae68960148ca7d",
+    {
+        header: Authorization="apikey beeaae5d371b40ff9bae68960148ca7d"
+    })
+
+    let stocksResults =await stockResults.json()
+    console.log(stocksResults)
 }
 
 main()
